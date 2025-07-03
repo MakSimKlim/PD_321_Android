91afdedc742a46345b1377397b06b16ba13ae150
MakSimKlim <83maxomus@mail.ru>
2025-07-03 15:09:53 +0500

    Exercises #3: New line is added to the list under the number DONE

--- MyFirstApplication/app/src/main/java/ru/mkits/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/ru/mkits/myfirstapplication/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
 
      */
-
+    /*
     //Exercise #2
     TextView textView;
     @Override
@@ -49,7 +49,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.long_text_view);
+    }
+    }
+     */
 
 
+    //Exercise #3
+    EditText editText;
+    Button sendButton;
+    TextView textView;
+    int counter = 1;  // Счётчик для нумерации строк
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Инициализация элементов
+        editText = findViewById(R.id.edit_message);
+        sendButton = findViewById(R.id.my_button);
+        textView = findViewById(R.id.scroll_message);
+
+        // Обработчик нажатия кнопки
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.append(counter + ". " + editText.getText() + "\n");    // Получаем текст и добавляем с номером в TextView
+                counter++;                                                      // Увеличиваем счетчик
+                editText.setText("");                                           // Очищаем EditText
+            }
+         });
     }
 }
