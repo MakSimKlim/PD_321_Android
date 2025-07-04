bcd4f707454a105462473d2edcf9436cede5b140
MakSimKlim <83maxomus@mail.ru>
2025-07-04 15:40:58 +0500

    TaxCalculator: Update #1 for App is DONE

--- TaxCalculator/app/src/main/java/ru/mkits/taxcalculator/MainActivity.java
+++ TaxCalculator/app/src/main/java/ru/mkits/taxcalculator/MainActivity.java
@@ -20,12 +20,16 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     TextView titleTextView1;
-    Button btnLanguage;
+    TextView keyRateTitleTextView;
+    TextView keyRateTextView;
     EditText keyRateEditText;
+    SeekBar seekBar;
+    TextView incomeTitleTextView;
     EditText incomeEditText;
     Button calculateButton;
     TextView titleTextView;
     TextView taxTextView;
+    TextView averageTaxIncomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +38,36 @@ public class MainActivity extends AppCompatActivity {
 
         // Инициализируем элементы
         titleTextView1 = findViewById(R.id.titleTextView1);
-        btnLanguage = findViewById(R.id.btnLanguage);
+        keyRateTitleTextView = findViewById(R.id.keyRateTitleTextView);
+        //keyRateTextView = findViewById(R.id.keyRateTextView);
         keyRateEditText = findViewById(R.id.keyRateEditText);
+        seekBar = findViewById(R.id.seekBar);
+        incomeTitleTextView = findViewById(R.id.incomeTitleTextView);
         incomeEditText = findViewById(R.id.incomeEditText);
         calculateButton = findViewById(R.id.calculateButton);
         titleTextView = findViewById(R.id.titleTextView);
         taxTextView = findViewById(R.id.taxTextView);
+        averageTaxIncomeTextView = findViewById(R.id.averageTaxIncomeTextView);
 
-        // Обработчик нажатия кнопки смены языка
 
-        btnLanguage.setOnClickListener(new View.OnClickListener() {
+        // Обработчик SeekBar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                String currentLang = Locale.getDefault().getLanguage();
-                String newLang = currentLang.equals("ru") ? "en" : "ru";
-                //LocaleHelper.setLocale(this, newLang);
-                recreate();
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Преобразуем целое число (0-100) в дробное (0.00-1.00)
+                float value = progress / 100.0f;
+                //keyRateTextView.setText(String.format("%.2f", value));
+                keyRateEditText.setText(String.format("%.2f", value));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
@@ -57,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Получаем максимальную ставку ЦентоБанка за налоговый период
+                // Получаем максимальную ставку ЦентроБанка за налоговый период
                 float keyRateAmount = Float.parseFloat(keyRateEditText.getText().toString());
                 // Получаем доход от вкладов за год
                 float incomeAmount = Float.parseFloat(incomeEditText.getText().toString());
@@ -75,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
                 // Устанавливаем значения в Toast
                 Toast.makeText(MainActivity.this, "Non-taxable min interest income:\n" + nonTaxableMinimumIncome, Toast.LENGTH_LONG).show();
                 Toast.makeText(MainActivity.this, "Exceeding the min interest income:\n" + (exceedMinimumInterestIncome > 0 ? exceedMinimumInterestIncome : "0"), Toast.LENGTH_LONG).show();
+
+                // Вычисляем усредненный процент налога от количества полученного дохода с депозитов
+                averageTaxIncomeTextView.setText(String.format("average tax from the amount of income in %%: %.4f", tax * 100 / incomeAmount) + "%");
             }
         });
     }
