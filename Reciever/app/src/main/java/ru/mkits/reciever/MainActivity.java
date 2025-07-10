9586a948ef85776f2b7df5c7644a8a6241b2c7cf
MakSimKlim <83maxomus@mail.ru>
2025-07-10 18:09:50 +0500

    Reciever App check internet type DONE

--- Reciever/app/src/main/java/ru/mkits/reciever/MainActivity.java
+++ Reciever/app/src/main/java/ru/mkits/reciever/MainActivity.java
@@ -1,20 +1,76 @@
 package ru.mkits.reciever;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlertDialog alertDialog;
+    boolean isDialogVisible;
+    TextView statusTextView;
+    TextView logTextView;
+    NetworkChangeReceiver networkReceiver;
+
+    ArrayList<String> logList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusTextView = findViewById(R.id.statusTextView);
+        logTextView = findViewById(R.id.logTextView);
+        networkReceiver = new NetworkChangeReceiver(this);
+        IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(networkReceiver, filter);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkReceiver);
+    }
 
+    public void showConnectDialog() {
+        if (!isDialogVisible) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Нет интернета")
+                    .setMessage("Проверь соединение")
+                    .setCancelable(false);
+            alertDialog = builder.create();
+            alertDialog.show();
+            isDialogVisible = true;
+        }
+    }
+    public void dismissConnectionDialog(){
+        if (isDialogVisible && alertDialog!=null) {
+            alertDialog.dismiss();
+            isDialogVisible = false;
+        }
+    }
+    public void updateStatus(String status){
+        statusTextView.setText(status);
+
+    }
+    public void addLogEntry(String message){
+        String timestamp = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+        String entry = "["+timestamp + "] " +message;
+        logList.add(0,entry);
+        updateLogView();
+    }
+    public void updateLogView(){
+        StringBuilder sb = new StringBuilder();
+        for (String entry:logList){
+            sb.append(entry).append("\n");
+        }
+        logTextView.setText(sb.toString());
     }
 }
